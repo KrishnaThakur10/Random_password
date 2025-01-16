3fb2b7e66c041f2306b2148eb68822e5ae71c7da
KrishnaThakur10 <krishnathakur27082005@gmail.com>
2025-01-16 14:54:22 +0530

    Modify the app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 
 function App() {
+  const [length, setLength] = useState(8)
+  const [numberAllow, setNumberAllow] = useState(false)
+  const [charAllow, setCharAllow] = useState(false)
+  const [password, setPassword] = useState("")
+
+  const passwordGenerator = useCallback(()=>{
+   const pass = ""
+   const str = "ABCDEFIJKLMNOPQRSTUVWXYZabcdefijklmnopqrstuvwxyz"
+
+   if(numberAllow) str+= "0123456789"
+   if(charAllow) str+= "!@#$%^&*()[]{}:;<>,./?\|~`_+-="
+
+  }, [length, numberAllow, charAllow, password])
+
   return (
     <div>
       hello password
